2c4732e53abbbe4c11d6838f07efdfbfced3111f
shauchun <cscjim0530@gmail.com>
2023-08-24 00:24:03 +0800

    hello

--- rabbitTest/web/lifetime.py
+++ rabbitTest/web/lifetime.py
@@ -1,42 +1,13 @@
-import logging
 from typing import Awaitable, Callable
 
 from fastapi import FastAPI
-from rabbitTest.settings import settings
-from prometheus_fastapi_instrumentator.instrumentation import \
-    PrometheusFastApiInstrumentator
-from rabbitTest.services.rabbit.lifetime import (init_rabbit,
-                                                                    shutdown_rabbit)
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy.orm import sessionmaker
-from rabbitTest.db.meta import meta
-from rabbitTest.db.models import load_all_models
+from prometheus_fastapi_instrumentator.instrumentation import (
+    PrometheusFastApiInstrumentator,
+)
+
+from app.services.kafka.lifetime import init_kafka, shutdown_kafka
 
 
-def _setup_db(app: FastAPI) -> None:  # pragma: no cover
-    """
-    Creates connection to the database.
-
-    This function creates SQLAlchemy engine instance,
-    session_factory for creating sessions
-    and stores them in the application's state property.
-
-    :param app: fastAPI application.
-    """
-    engine = create_async_engine(str(settings.db_url), echo=settings.db_echo)
-    session_factory = async_sessionmaker(
-        engine,
-        expire_on_commit=False,
-    )
-    app.state.db_engine = engine
-    app.state.db_session_factory = session_factory
-async def _create_tables() -> None:  # pragma: no cover
-    """Populates tables in the database."""
-    load_all_models()
-    engine = create_async_engine(str(settings.db_url))
-    async with engine.begin() as connection:
-        await connection.run_sync(meta.create_all)
-    await engine.dispose()
 def setup_prometheus(app: FastAPI) -> None:  # pragma: no cover
     """
     Enables prometheus integration.
@@ -48,7 +19,9 @@ def setup_prometheus(app: FastAPI) -> None:  # pragma: no cover
     ).expose(app, should_gzip=True, name="prometheus_metrics")
 
 
-def register_startup_event(app: FastAPI) -> Callable[[], Awaitable[None]]:  # pragma: no cover
+def register_startup_event(
+    app: FastAPI,
+) -> Callable[[], Awaitable[None]]:  # pragma: no cover
     """
     Actions to run on application startup.
 
@@ -61,18 +34,16 @@ def register_startup_event(app: FastAPI) -> Callable[[], Awaitable[None]]:  # pr
 
     @app.on_event("startup")
     async def _startup() -> None:  # noqa: WPS430
-        app.middleware_stack = None
-        _setup_db(app)
-        await _create_tables()
-        init_rabbit(app)
+        # await init_kafka(app)
         setup_prometheus(app)
-        app.middleware_stack = app.build_middleware_stack()
         pass  # noqa: WPS420
 
     return _startup
 
 
-def register_shutdown_event(app: FastAPI) -> Callable[[], Awaitable[None]]:  # pragma: no cover
+def register_shutdown_event(
+    app: FastAPI,
+) -> Callable[[], Awaitable[None]]:  # pragma: no cover
     """
     Actions to run on application's shutdown.
 
@@ -82,9 +53,7 @@ def register_shutdown_event(app: FastAPI) -> Callable[[], Awaitable[None]]:  # p
 
     @app.on_event("shutdown")
     async def _shutdown() -> None:  # noqa: WPS430
-        await app.state.db_engine.dispose()
-        
-        await shutdown_rabbit(app)
+        # await shutdown_kafka(app)
         pass  # noqa: WPS420
 
     return _shutdown
