6e4efcc050acb25bc6bf574403b0caa553e649b7
shauchun <cscjim0530@gmail.com>
2023-08-24 01:08:52 +0800

    UnitTest

---
+++ rabbitTest/tests/test_db.py
@@ -0,0 +1,27 @@
+import asyncio
+import pytest
+from unittest.mock import patch, MagicMock
+from starlette.requests import Request
+from sqlalchemy.ext.asyncio import AsyncSession
+from hello.db.dependencies import (
+    get_db_session,
+)  # Replace with your actual module and function
+
+
+class MockAsyncSession:
+    async def commit(self):
+        pass
+
+    async def close(self):
+        pass
+
+
+@pytest.mark.asyncio
+@patch("hello.db.dependencies.AsyncSession", new=MockAsyncSession)
+async def test_get_db_session():
+    request_mock = MagicMock()
+    request_mock.app.state.db_session_factory = lambda: AsyncSession()
+
+    session = get_db_session(request_mock).__anext__()
+    hello = await session
+    assert isinstance(hello, AsyncSession)
